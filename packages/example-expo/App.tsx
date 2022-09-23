33df9c1e594bc93b3e4201c74f63115143d1c6d3
krit <krittanon@amity.co>
2022-09-23 17:59:00 +0700

    update peerDependencies

--- packages/example-expo/App.tsx
+++ packages/example-expo/App.tsx
@@ -1,13 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { hello } from "react-native-asc-ui-kit";
+import { RedBox } from "react-native-asc-ui-kit";
 
 export default function App() {
-  hello();
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <RedBox />
     </View>
   );
 }
--- packages/react-native-asc-ui-kit/index.tsx
+++ packages/react-native-asc-ui-kit/index.tsx
@@ -1,1 +1,8 @@
+import React from "react";
+import { View } from "react-native";
+
+export const RedBox = () => {
+  return <View style={{ width: 100, height: 100, backgroundColor: "red" }} />;
+};
+
 export const hello = () => console.log("Hello");
